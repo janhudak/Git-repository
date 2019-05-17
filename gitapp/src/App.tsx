56e3700e21606a3a150dfa3affcbfb57f4b3083c
Hudak Jan <hudakj@lynx.sk>
2019-05-17 08:49:25 +0200

    push to the new branch

--- gitapp/src/App.tsx
+++ gitapp/src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React New Branch
         </a>
       </header>
     </div>
